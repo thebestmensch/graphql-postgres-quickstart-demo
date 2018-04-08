8f748aceef912eff8edcf37980c91a02312a0ce4
helabbassi <helabbassi@kilix.fr>
2018-04-08 02:17:36 +0200

    Bump dependencies & fix resolvers

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # graphql-postgres-quickstart-demo
+
 GraphQL &amp; PostgreSQL Quickstart http://jamesmensch.com/tutorials/2017/graphql-and-postgresql-node-quickstart/
+
+https://medium.com/@james_mensch/node-js-graphql-postgresql-quickstart-91ffa4374663
--- schema.js
+++ schema.js
@@ -1,7 +1,7 @@
-const GraphQLSchema = require('graphql').GraphQLSchema;
-const makeExecutableSchema = require('graphql-tools').makeExecutableSchema;
+const GraphQLSchema = require("graphql").GraphQLSchema;
+const makeExecutableSchema = require("graphql-tools").makeExecutableSchema;
 
-const resolver = require('./resolver').resolver; // our resolver from resolver.js
+const resolvers = require("./resolver").resolver; // our resolver from resolver.js
 
 // define our user type
 // then define a users query, which must return an array type that optionally contains 0 or more User types
@@ -19,5 +19,5 @@ const typeDefs = `
 
 exports.schema = makeExecutableSchema({
   typeDefs,
-  resolver,
+  resolvers
 });
